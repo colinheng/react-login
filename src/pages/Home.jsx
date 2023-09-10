1f33e399093bea016ab677d842c58b0541ba278a
Colin Heng <colinheng@gmail.com>
2023-09-10 11:20:15 +0800

    For .mynotes file

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,17 +4,36 @@ function Home() {
 
   return (
     <>
-      <div className="container">
+      <div className="container mx-auto">
         <header>
-          <div className="header">
-            Miaow bids you welcome!
+          <div className="header ">
+            Miaow says hello!
           </div>
         </header>
 
         <main>
           <form>
-            <input type='email' className='emailInput' placeholder='email' id='email' autoComplete='email' />
-            <input type='password' className='passwordInput' placeholder='password' id='password' />
+            <div className="form-group">
+              <input 
+                type='email'
+                className='emailInput'
+                placeholder='email'
+                id='email'
+                autoComplete='email'
+                />
+            </div>
+            <div className="form-group">
+              <input 
+                type='password' 
+                className='passwordInput' 
+                placeholder='password' 
+                id='password' />
+            </div>
+            <div className="form-group">
+              <button className="btn btn-block">
+                Hello!
+              </button>
+            </div>
           </form>
         </main>
       </div>      
